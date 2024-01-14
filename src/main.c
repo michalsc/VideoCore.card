3e43f0e93fec0a599cc298857d8bebade9756ea3
Michal Schulz <michal.schulz@gmx.de>
2024-01-14 11:42:49 +0000

    fixed check for unicam.boot option

--- src/main.c
+++ src/main.c
@@ -668,7 +668,7 @@ static int InitCard(REGARG(struct BoardInfo* bi, "a0"), REGARG(const char **Tool
     APTR UnicamBase = VC4Base->vc4_UnicamBase;
 
     /* If Unicam was activated on boot, pre-select CSI switch mode */
-    if (UnicamGetConfig() && UNICAMF_BOOT) VC4Base->vc4_SwitchMode = CSI;
+    if ((UnicamGetConfig() & UNICAMF_BOOT) != 0) VC4Base->vc4_SwitchMode = CSI;
 
     for (;ToolTypes[0] != NULL; ToolTypes++)
     {
